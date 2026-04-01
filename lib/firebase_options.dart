25cb9f7365344e6811adc3cf56a0a091ad329a5e
scoroian <sebastiancoroian@gmail.com>
2026-04-01 22:38:49 +0200

    chore: add firebase_options stub (replace with flutterfire configure)

---
+++ lib/firebase_options.dart
@@ -0,0 +1,39 @@
+// GENERATED FILE — replace by running: flutterfire configure
+// ignore_for_file: lines_longer_than_80_chars, avoid_classes_with_only_static_members
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for this platform.',
+        );
+    }
+  }
+
+  // TODO: Replace with real values from flutterfire configure
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'REPLACE_ME',
+    appId: 'REPLACE_ME',
+    messagingSenderId: 'REPLACE_ME',
+    projectId: 'toka-dev',
+    storageBucket: 'toka-dev.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'REPLACE_ME',
+    appId: 'REPLACE_ME',
+    messagingSenderId: 'REPLACE_ME',
+    projectId: 'toka-dev',
+    storageBucket: 'toka-dev.appspot.com',
+    iosClientId: 'REPLACE_ME',
+    iosBundleId: 'com.toka',
+  );
+}

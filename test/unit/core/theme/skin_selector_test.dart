8bd1f78605244a4c67fdbeccb727e240aa09497c
scoroian <sebastiancoroian@gmail.com>
2026-04-14 15:48:04 +0200

    fix(skin): nombres de test en español

--- test/unit/core/theme/skin_selector_test.dart
+++ test/unit/core/theme/skin_selector_test.dart
@@ -2,12 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:toka/core/theme/app_skin.dart';
 
 void main() {
-  test('AppSkin.v2 exists and SkinConfig.current is v2 by default', () {
+  test('AppSkin.v2 existe y SkinConfig.current es v2 por defecto', () {
     expect(AppSkin.values, contains(AppSkin.v2));
     expect(SkinConfig.current, AppSkin.v2);
   });
 
-  test('SkinConfig.current can be changed to material', () {
+  test('SkinConfig.current se puede cambiar a material', () {
     SkinConfig.current = AppSkin.material;
     expect(SkinConfig.current, AppSkin.material);
     SkinConfig.current = AppSkin.v2; // restore
